c7a018e5867f16eef7318ce0e8af0d742a32cf52
xibsx <xibstech@gmail.com>
2025-08-09 22:31:29 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,33 @@
+{
+  "name": "xa-whatsapp-bot",
+  "version": "1.0.0",
+  "description": "WhatsApp Bot using Baileys",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/xibsx/xa.git"
+  },
+  "keywords": [
+    "whatsapp",
+    "bot",
+    "baileys"
+  ],
+  "author": "xibsx",
+  "license": "MIT",
+  "bugs": {
+    "url": "https://github.com/xibsx/xa/issues"
+  },
+  "homepage": "https://github.com/xibsx/xa#readme",
+  "dependencies": {
+    "@whiskeysockets/baileys": "^6.4.0",
+    "pino": "^8.16.0",
+    "pino-pretty": "^10.2.0"
+  },
+  "engines": {
+    "node": ">=20.0.0"
+  }
+}
